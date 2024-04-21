a248c1d57ad7eecd13e134bf3d5599f1f12ec3a6
Rajnish Kumar Saphi <Hsinjar2001@gmail.com>
2024-04-21 14:03:28 +0545

    First Commit

---
+++ implimantion_case.py
@@ -0,0 +1,47 @@
+#camal case
+pythonProgramming= 'python'
+javaProgramming = 'java'
+print(id(pythonProgramming))
+print(id(javaProgramming))
+
+#pascal case
+PythonProgramming = 'python'
+JavaProgramming = 'java'
+print(id(PythonProgramming))
+print(id(JavaProgramming))
+
+#shake case
+python_programming = 'python'
+java_programming = 'java'
+print(id (python_programming))
+print(id(java_programming))
+
+#flat case
+pythonprogramming = 'python'
+javaprogramming = 'java'
+print(id(pythonprogramming))
+print(id(javaprogramming))
+
+#upper case
+PYTHONPROGRAMMING = 'python'
+JAVAPROGRAMMING = 'java'
+print(id(PYTHONPROGRAMMING))
+print(id(JAVAPROGRAMMING))
+
+#came/ shake case
+python_Programming = 'python'
+java_Programming = 'java'
+print(id(python_Programming))
+print(id(java_Programming))
+
+# pascal snake case
+Python_Programming =' python'
+Java_Programming = 'java'
+print(id(Python_Programming))
+print(id(Java_Programming))
+
+# stream shank case
+PYTHON_PROGRAMMING = 'python'
+JAVA_PROGRAMMING = 'java'
+print(id(PYTHON_PROGRAMMING))
+print(id(JAVA_PROGRAMMING))
